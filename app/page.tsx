2543a117c8bde9ecfe822da739153e98a33dc15e
Charles <charleszecevic@gmail.com>
2025-11-28 20:27:13 +0100

    debug second try

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { AllCourse } from "./components/AllCourse";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
